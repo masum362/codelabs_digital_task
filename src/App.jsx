fca6ee521a76a522dcf3c31a9d9f6c644fed88a0
masum362 <masumahmed64077@gmail.com>
2024-07-03 22:16:49 +0600

    add: hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import Footer from "./components/footer/Footer"
+import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
 
 
 function App() {
   return (
     <>
-     <div className="w-[90%] mx-auto">
-      <header>
-      <Navbar />
-      </header>
-     </div>
-     <Footer />
+      <div className="w-[90%] mx-auto">
+        <header>
+          <Navbar />
+          <Hero />
+        </header>
+      </div>
+      <Footer />
     </>
   )
 }
